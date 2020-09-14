42925e42bc0045818ce944fe4fd7efa17426b87e
beskep <beskep@naver.com>
2020-09-14 14:32:11 +0900

    members
    결재자가 4명이 아닌 경우에도 적용

--- src/draft/daou_draft.py
+++ src/draft/daou_draft.py
@@ -21,11 +21,9 @@ STAMP_PATH = ROOT_DIR / 'src/stamp_approved.png'
 
 class DaouDraft:
 
-  __label = {'docNo', 'draftDate', 'draftDept', 'draftUser'}
-  __sign_member_count = 4
-  __default_rank = ('사원', '과장', '본부장', '대표이사')
-  __ymd_format = '%Y/%m/%d'
-  __md_format = '%m/%d'
+  _label = {'docNo', 'draftDate', 'draftDept', 'draftUser'}
+  _ymd_format = '%Y/%m/%d'
+  _md_format = '%m/%d'
 
   def __init__(self, path) -> None:
     self._path = path
@@ -35,6 +33,7 @@ class DaouDraft:
         __name__, self.__class__.__qualname__))
 
     self._member, self._rank, self._name, self._date = self._sign_members()
+    self._sign_count = len(self._member)
 
   @property
   def soup(self):
@@ -59,19 +58,29 @@ class DaouDraft:
 
     sign_date = [_find(x, 'sign_date') for x in sign_member]
 
-    assert len(sign_rank) == self.__sign_member_count
-    assert len(sign_name) == self.__sign_member_count
-    assert len(sign_date) == self.__sign_member_count
-
     return sign_rank, sign_name, sign_date
 
+  def _sign_members(self):
+    sign_members = self.soup.find_all(name='span',
+                                      attrs={'class': 'sign_member'})
+
+    sign_ranks, sign_names, sign_dates = self._sign_members_helper(sign_members)
+
+    # sign_rank_string = [str(x.string).strip() for x in sign_ranks]
+    # if sign_rank_string != list(self.__default_rank):
+    #   self._logger.debug('직급 순서가 예상과 다름: {}'.format(sign_rank_string))
+
+    return sign_members, sign_ranks, sign_names, sign_dates
+
   def _fill_sign_member(self):
+    """전결이 존재하는 경우 빈 결재자 형식을 채워넣음"""
+
     sign_member, _, sign_name, _ = self._sign_members()
 
     assert sign_name[0] is not None
     doc = self.soup.decode(False)
 
-    for idx in range(1, self.__sign_member_count):
+    for idx in range(1, self._sign_count):
       # TODO: 그냥 bs4.Tag.replace_with로 바꿀 수 없나...
       if sign_name[idx] is None:
         sm1 = sign_member[idx].decode()
@@ -84,38 +93,23 @@ class DaouDraft:
     self._member, self._rank, self._name, self._date = self._sign_members()
 
   def _fix_sign_date(self):
+    """전결이 존재하는 경우 빈 날짜 형식을 채워넣음"""
+
     _, _, _, sign_date = self._sign_members()
 
     first_date = datetime.datetime.strptime(sign_date[0].string.strip(),
-                                            self.__ymd_format)
+                                            self._ymd_format)
 
-    for idx in range(1, self.__sign_member_count):
+    for idx in range(1, self._sign_count):
       text = sign_date[idx].string.strip()
       if '전결' in text:
         date = datetime.datetime.strptime(text.replace('(전결)', ''),
-                                          self.__md_format)
+                                          self._md_format)
         fixed_date = datetime.date(first_date.year, date.month, date.day)
-        fixed_text = datetime.date.strftime(fixed_date, self.__ymd_format)
+        fixed_text = datetime.date.strftime(fixed_date, self._ymd_format)
         sign_date[idx].string.replace_with(fixed_text)
 
     self._member, self._rank, self._name, self._date = self._sign_members()
-
-  def _sign_members(self):
-    sign_member = self.soup.find_all(name='span',
-                                     attrs={'class': 'sign_member'})
-    assert len(sign_member) == self.__sign_member_count
-
-    sign_rank, sign_name, sign_date = self._sign_members_helper(sign_member)
-
-    sign_rank_string = [str(x.string).strip() for x in sign_rank]
-    if sign_rank_string != list(self.__default_rank):
-      self._logger.debug('직급 순서가 예상과 다름: {}'.format(sign_rank_string))
-
-    assert len(sign_rank) == self.__sign_member_count
-    assert len(sign_name) == self.__sign_member_count
-    assert len(sign_date) == self.__sign_member_count
-
-    return sign_member, sign_rank, sign_name, sign_date
 
   def fix_sign(self):
     if any(x is None for x in self._name):
@@ -128,24 +122,41 @@ class DaouDraft:
       f.write(str(self._soup.prettify()))
 
   def change_label(self, label: str, text: str):
-    if label not in self.__label:
+    if label not in self._label:
       raise ValueError('Invalid label: {}'.format(label))
 
     tag = self.soup.find(name='span',
                          attrs={'data-dsl': '{{label:' + str(label) + '}}'})
     tag.string.replace_with(text)
 
+  def _warn_sign_count(self, label, values):
+    self._logger.warning('''결재 정보 불일치
+기안 원본의 결재자 수: {0}
+입력받은 {1} 수: {2} {3}
+순서대로 {1} 정보를 채웁니다.'''.format(self._sign_count, label, len(values),
+                             str(tuple(values))))
+    return
+
   def change_sign_rank(self, ranks: list):
+    if self._sign_count != len(ranks):
+      self._warn_sign_count(label='결재자 직급', values=ranks)
+
     for tag, rank in zip(self._rank, ranks):
       if tag and rank:
         tag.string.replace_with(rank)
 
   def change_sign_name(self, names: list):
+    if self._sign_count != len(names):
+      self._warn_sign_count(label='결재자 이름', values=names)
+
     for tag, name in zip(self._name, names):
       if tag and name:
         tag.string.replace_with(name)
 
   def change_sign_date(self, dates: list):
+    if self._sign_count != len(dates):
+      self._warn_sign_count(label='결재일', values=dates)
+
     for tag, date in zip(self._date, dates):
       if tag and date:
         tag.string.replace_with(date)
--- tests/test_draft.py
+++ tests/test_draft.py
@@ -11,37 +11,48 @@ from draft import daou_draft
 
 
 @pytest.fixture
-def draft():
-  html_path = './documents/example.html'
+def draft_blank():
+  html_path = './documents/example_blank.html'
   d = daou_draft.DaouDraft(html_path)
   return d
 
 
-def test_draft_label(draft):
-  test: bs4.Tag = draft.soup.find(name='span',
-                                  attrs={'data-dsl': '{{label:draftUser}}'})
+@pytest.fixture
+def draft_five_members():
+  html_path = './documents/example_five members.html'
+  d = daou_draft.DaouDraft(html_path)
+  return d
+
+
+def test_draft_label(draft_blank):
+  test: bs4.Tag = draft_blank.soup.find(
+      name='span', attrs={'data-dsl': '{{label:draftUser}}'})
   test.string.replace_with('test')
 
   res_path = './pdf/test.html'
-  draft.save(res_path)
+  draft_blank.save(res_path)
   assert os.path.exists(res_path)
 
 
-def test_draft_user(draft):
-  members = draft.soup.find_all(name='span', attrs={'class': 'sign_member'})
-  sign_rank_wrap = draft.soup.find_all(name='span',
-                                       attrs={'class': 'sign_rank_wrap'})[0]
+def test_draft_user(draft_blank):
+  members = draft_blank.soup.find_all(name='span',
+                                      attrs={'class': 'sign_member'})
+  sign_rank_wrap = draft_blank.soup.find_all(name='span',
+                                             attrs={'class': 'sign_rank_wrap'
+                                                   })[0]
   children = list(members[0].children)
   assert sign_rank_wrap in children
 
 
-def test_draft_fill_sign_member(draft: daou_draft.DaouDraft):
-  sign_member, sign_rank, sign_name, sign_date = draft._sign_members()
+def test_draft_fill_sign_member(draft_blank: daou_draft.DaouDraft):
+  sign_member, sign_rank, sign_name, sign_date = draft_blank._sign_members()
   assert any(x is None for x in sign_name)
 
-  draft._fill_sign_member()
-  sign_member = draft.soup.find_all(name='span', attrs={'class': 'sign_member'})
-  sign_rank, sign_name, sign_date = draft._sign_members_helper(sign_member)
+  draft_blank._fill_sign_member()
+  sign_member = draft_blank.soup.find_all(name='span',
+                                          attrs={'class': 'sign_member'})
+  sign_rank, sign_name, sign_date = draft_blank._sign_members_helper(
+      sign_member)
 
   assert id(sign_member[2]) != id(sign_member[3])
   assert all(x.string.strip() for x in sign_rank)
@@ -49,22 +60,22 @@ def test_draft_fill_sign_member(draft: daou_draft.DaouDraft):
   assert all(x.string.strip() for x in sign_date)
 
 
-def test_fix_sign_date(draft: daou_draft.DaouDraft):
-  sign_member, sign_rank, sign_name, sign_date = draft._sign_members()
-  assert any(x is None for x in sign_name)
+def test_fix_sign_date(draft_five_members: daou_draft.DaouDraft):
+  members, ranks, names, dates = draft_five_members._sign_members()
 
-  draft._fill_sign_member()
-  sign_member = draft.soup.find_all(name='span', attrs={'class': 'sign_member'})
-  sign_rank, sign_name, sign_date = draft._sign_members_helper(sign_member)
+  draft_five_members._fill_sign_member()
+  members = draft_five_members.soup.find_all(name='span',
+                                             attrs={'class': 'sign_member'})
+  ranks, names, dates = draft_five_members._sign_members_helper(members)
 
-  draft._fix_sign_date()
-  for d in draft._date:
+  draft_five_members._fix_sign_date()
+  for d in draft_five_members._date:
     assert d.string
-    datetime.strptime(d.string.strip(), draft._DaouDraft__ymd_format)
+    datetime.strptime(d.string.strip(), draft_five_members._ymd_format)
 
 
-def test_img(draft: daou_draft.DaouDraft):
-  draft.change_image_source()
+def test_img(draft_blank: daou_draft.DaouDraft):
+  draft_blank.change_image_source()
 
 
 def test_runner():
